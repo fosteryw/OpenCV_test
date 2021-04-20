9610b9cf5cf57cea1d0209036a2dbfd5e6b5823f
foster <koalavalera@mail.ru>
2021-04-20 18:20:37 +0300

    Fix code

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import numpy as np
 import cv2
-
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+while (True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('Video', frame)
