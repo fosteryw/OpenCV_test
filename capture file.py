c3b044105d86246fbc38c00b31dc6158b9bb93bb
foster <koalavalera@mail.ru>
2021-04-20 18:29:05 +0300

    Fix code

--- capture file.py
+++ capture file.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import datetime
 
 filename = 'video.avi'
 frames_per_second = 24.0
@@ -44,12 +45,22 @@ def get_video_type(filename):
 cap = cv2.VideoCapture(0)
 out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cap, res))
 
-while True:
+# while True:
+#     ret, frame = cap.read()
+#     out.write(frame)
+#     cv2.imshow('frame', frame)
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+
+time_start = datetime.datetime.now()
+time_now = datetime.datetime.now()
+while time_now.second - time_start.second < 20:
     ret, frame = cap.read()
     out.write(frame)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    time_now = datetime.datetime.now()
 
 cap.release()
 out.release()
